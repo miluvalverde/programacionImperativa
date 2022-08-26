3f1d11180bfd81e6e41bbab9a852a2dc14ac5d80
miluvalverde <miluvalverde11@gmail.com>
2022-08-26 12:01:50 -0300

    Agregar resolución ejericio 2

--- clase11.js
+++ clase11.js
@@ -87,3 +87,32 @@ let banco = {
 
 
 
+// ============== EJERCICIO 2 - PROPIEDAD ÚNICA ==============
+
+// Crear una función llamada propiedadUnica que reciba un arreglo de objetos como
+// parámetro y un string. Esta deberá retornar un nuevo arreglo de objetos, teniendo como
+// parámetro la propiedad que fue pasada como string.
+
+let array = [ 
+    { 
+        nombre: "Lean", 
+        edad: 27, 
+    }, 
+    { 
+        nombre: "Eze", 
+        edad: 49,
+    } 
+]
+
+function propiedadUnica(arr, str) {
+    let nuevoArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        nuevoArray.push({
+            [str]: arr[i][str],
+        });
+    }
+    console.log(nuevoArray);
+    return nuevoArray;
+}
+propiedadUnica(array, "edad")
+
