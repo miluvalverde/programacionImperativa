1c591a1019a67e603d521e959f90752b4798c6f2
miluvalverde <miluvalverde11@gmail.com>
2022-08-29 19:04:05 -0300

    Agregar ejercicio 3 y arreglar cuentas de aumento de precio en todos los vehículos

--- clase7.js
+++ clase7.js
@@ -32,16 +32,16 @@ const prompt = require("prompt-sync")({ sigint: true });
 //         aumento = 25;
 //     }
 //     if (vehiculo == "coche") {
-//         litrosConsumidos * 86 + aumento
-//         console.log("Debes pagar en total " + litrosConsumidos + " pesos");
+//         aumento = litrosConsumidos * 86
+//         console.log("Debes pagar en total " + aumento + " pesos");
 //         return litrosConsumidos;
 //     } else if (vehiculo == "moto") {
-//         litrosConsumidos * 70 + aumento
-//         console.log("Debes pagar en total " + litrosConsumidos + " pesos");
+//         aumento = litrosConsumidos * 70
+//         console.log("Debes pagar en total " + aumento + " pesos");
 //         return litrosConsumidos;
 //     } else if (vehiculo == "autobus") {
-//         litrosConsumidos * 55 + aumento
-//         console.log("Debes pagar en total " + litrosConsumidos + " pesos");
+//         aumento = litrosConsumidos * 55
+//         console.log("Debes pagar en total " + aumento + " pesos");
 //         return litrosConsumidos;
 //     }
 // }
@@ -51,10 +51,57 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 // EJERCICIO 3
 
+// let baseSandwich = prompt("Qué tipo de sandwich quiere (Pollo, Carne o Vegetariano)? ");
+// let panSandwich = prompt("Qué tipo de pan quiere? Opciones (coloque el número indicado en paréntesis): blanco c/orégano y parmesano (1), negro c/avena (2) o sin gluten (3)? ");
+// let preguntaQueso = prompt("Desea que el sandwich posea queso? (Responder con un true o false) ");
+// let preguntaTomate = prompt("Desea que el sandwich posea tomate? (Responder con un true o false) ");
+// let preguntaLechuga = prompt("Desea que el sandwich posea lechuga? (Responder con un true o false) ");
+// let preguntaCebolla = prompt("Desea que el sandwich posea cebolla? (Responder con un true o false) ");
+// let preguntaMayonesa = prompt("Desea que el sandwich posea mayonesa? (Responder con un true o false) ");
+// let preguntaMostaza = prompt("Desea que el sandwich posea mostaza? (Responder con un true o false) ");
 
-
-
-
+// function sandwiches(strBase, strPan, booleanQueso, booleanTomate, booleanLechuga, booleanCebolla, booleanMayonesa,
+//     booleanMostaza) {
+//     let precio = 0;
+//     if (strBase == "pollo") {
+//         precio = 150;
+//     } else if (strBase == "carne") {
+//         precio = 200;
+//     } else if (strBase == "vegetariano") {
+//         precio = 100
+//     } else {
+//         console.log("Ingrese otra vez una base de sandwich de las especificadas")
+//     }
+//     if (strPan == "1") {
+//         precio += 50;
+//     } else if (strPan == "2") {
+//         precio += 60;
+//     } else if (strPan == "3") {
+//         precio += 75;
+//     } else {
+//         console.log("Ingrese otra vez la base y el tipo de pan especificados")
+//     }
+//     if (preguntaQueso === "true") {
+//         precio += 20;
+//     }
+//     if (preguntaTomate == "true") {
+//         precio +=15;
+//     }
+//     if (preguntaLechuga == "true") {
+//         precio +=10;
+//     }
+//     if (preguntaCebolla == "true") {
+//         precio +=15;
+//     }
+//     if (preguntaMayonesa == "true") {
+//         precio +=5;
+//     }
+//     if (preguntaMostaza == "true") {
+//         precio +=5;
+//     }
+//     console.log(`El precio total del sandwich elegido es ${precio}`);
+// }
+// sandwiches(baseSandwich, panSandwich, preguntaQueso, preguntaTomate, preguntaLechuga, preguntaCebolla, preguntaMayonesa, preguntaMostaza);
 
 
 // EJERCICIO 4
