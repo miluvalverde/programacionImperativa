35384e3644618337c55ba054210bfdc465ec9be7
miluvalverde <miluvalverde11@gmail.com>
2022-09-14 13:58:36 -0300

    Agregar ejercicios resueltos de Bucles y Repeticiones

--- repaso_final.js
+++ repaso_final.js
@@ -357,22 +357,40 @@ let valoracion = "Muy mala";
 // =================== Bucles y repeticiones ===================
 // 1) Crear una función que pida un valor por parámetro y muestre los 10
 // números siguientes.
+function numerosSiguientes (num) {
+    for (i = num; i < (num + 10); i++) {
+        console.log(i + 1);
+    }
+};
+// numerosSiguientes(10);
 
 
 // 2) Imprimir los números entre el 5 y el 20, saltando de tres en tres.
+// for (i = 5; i <= 20; i++) {
+//     i += 3;
+//     console.log(i);
+// }
 
 
 // 3) Escribir un programa que muestre la sumatoria de todos los números
 // entre el 0 y el 100
+let suma = 0;
+for (let i = 0; i <= 100; i++) {
+    suma += i;
+}
+// console.log(suma);
 
 
 // 4) Dado un número entero positivo, mostrar su factorial. El factorial de
 // un número se obtiene multiplicando todos los números enteros
 // positivos que hay entre el 1 y ese número.
+let numero = 4;
+let factorial = 1;
+for (let i = 1; i <= numero; i++) {
+    factorial *= i; 
+}
+// console.log(factorial);
 
-
-// 5) Crear una función que muestre todos los números de la secuencia de
-// fibonacci hasta el valor ingresado por parámetro.
 
 
 
