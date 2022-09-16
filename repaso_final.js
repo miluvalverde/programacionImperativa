5f4d6ef9c214095f868d0c42341e312a4311ce41
miluvalverde <miluvalverde11@gmail.com>
2022-09-15 22:00:44 -0300

    Agregar ejercicios resueltos de Preparando el cerebro

--- repaso_final.js
+++ repaso_final.js
@@ -847,43 +847,106 @@ function digitalHouse (num1, num2) {
 
 // =================== ¡Preparando el cerebro! ===================
 // Loop de pares
-// Deberás crear una función llamada loopDePares que reciba como parámetro un número y haga un loop de 0 a 100 mostrando en la consola cada número del loop. En caso de que el número de la iteración sumado con el número pasado por parámetro sea par, mostrará en la consola: “El número X es par”.
-// Loop de impares con palabra
-// Deberás crear una función llamada loopDeImpares que reciba como parámetros un número y una palabra, y haga un loop de 0 a 100 mostrando en la consola cada número del loop. Luego, modificar el código para que, en caso de que ese número sumado con el número pasado por parámetro sea impar, muestre en la consola la palabra pasada por parámetro.
+// Deberás crear una función llamada loopDePares que reciba como parámetro un número 
+// y haga un loop de 0 a 100 mostrando en la consola cada número del loop. 
+// En caso de que el número de la iteración sumado con el número pasado por 
+// parámetro sea par, mostrará en la consola: “El número X es par”.
+function loopDePares (num) {
+    for (let i = 0; i <= 100; i++) {
+        if ((i + num) % 2 === 0) {
+            console.log(`El número ${i} es par`);
+        } else {
+            console.log(i);
+        }
+    }
+};
+// loopDePares(5);
 
 
 // Sumatoria
-// Deberás crear una función llamada sumatoria que reciba un número como parámetro y que devuelva la sumatoria de todos sus números anteriores, incluso ese mismo. Ejemplo:
+// Deberás crear una función llamada sumatoria que reciba un número como parámetro 
+// y que devuelva la sumatoria de todos sus números anteriores, incluso ese mismo. 
+// Ejemplo:
 // sumatoria(3) debe retornar 6 porque hace (1+2+3)
 // sumatoria(8) debe retornar 36
+function sumatoria (num) {
+    let suma = 0;
+    for (let i = 0; i <= num; i++) {
+        suma += i;
+    }
+    return suma
+};
+// console.log(sumatoria(8));
 
 
 // Nuevo arreglo
-// Deberás crear una función llamada nuevoArreglo que reciba un número como parámetro y que devuelva un nuevo arreglo con tantos elementos como el número que le hayas pasado. Ejemplo:
+// Deberás crear una función llamada nuevoArreglo que reciba un número como parámetro 
+// y que devuelva un nuevo arreglo con tantos elementos como el número que le hayas 
+// pasado. Ejemplo:
 // nuevoArreglo(5) debe retornar [1,2,3,4,5]
 // nuevoArreglo(10) debe retornar [1,2,3,4,5,6,7,8,9,10]
+function nuevoArreglo (num) {
+    let nuevoArr = [];
+    for (let i = 1; i <= num; i++) {
+        nuevoArr.push(i);
+    };
+    return nuevoArr
+};
+// console.log(nuevoArreglo(5));
 
 
 // Similar String.split()
-// Deberás crear una función llamada split que reciba un string y simule el comportamiento de la función original. Si no estás seguro de cómo funciona, Google puede ayudarte. Importante: no podés usar el String.split(). Ejemplo:
+// Deberás crear una función llamada split que reciba un string y simule el 
+// comportamiento de la función original. Si no estás seguro de cómo funciona, 
+// Google puede ayudarte. Importante: no podés usar el String.split(). Ejemplo:
 // split(“hola”) debe retornar [“h”,”o”,”l”,”a”]
 // split(“chau”) debe retornar [“c”,”h”,”a”,”u”]
+function split (str) {
+    let nuevoArr = [];
+    for (let i = 0; i < str.length; i++) {
+        nuevoArr.push(str[i]);
+    }
+    return nuevoArr
+}
+// console.log(split("hola"));
 
 
 // Manejando dos arreglos
-// Deberás crear una función llamada arrayHandler que reciba dos arreglos de igual largo como parámetros y muestre en la consola “Soy {elemento de array 1} y yo soy {elemento de array 2}”. Ejemplo:
+// Deberás crear una función llamada arrayHandler que reciba dos arreglos de 
+// igual largo como parámetros y muestre en la consola “Soy {elemento de array 1} 
+// y yo soy {elemento de array 2}”. Ejemplo:
 // arrayHandler([1,2,3,4], [“h”,”o”,”l”,”a”]) debe mostrar: 
 // Soy 1 y yo soy h
 // Soy 2 y yo soy o
 // Soy 3 y yo soy l
 // Soy 4 y yo soy a
+let numerosCuatro = [1,2,3,4];
+let hola = ["h","o","l","a"];
+function arrayHandler (arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        console.log(`Soy ${arr1[i]} y yo soy ${arr2[i]}`);
+    }
+};
+// arrayHandler(numerosCuatro, hola);
 
 
 // Palíndromo
-// Deberás crear una función llamada palindromo que indique si una palabra es palíndroma o no. Debe retornar “true” en caso de que lo sea, y “false” en caso de que no. Ejemplo:
+// Deberás crear una función llamada palindromo que indique si una 
+// palabra es palíndroma o no. Debe retornar "true” en caso de que lo sea, 
+// y “false” en caso de que no. Ejemplo:
 // palindromo(“anilina”) debe retornar true
 // palindromo(“Ana”) debe retornar true
 // palindromo(“Enrique”) debe retornar false
+
+function palindromo (str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != str[str.length - i - 1]) {
+            return false
+        };
+    };
+    return true
+};
+// console.log(palindromo("ansa"));
 
 
 
